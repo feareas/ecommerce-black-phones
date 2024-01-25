2b933a10dc4615836d2a0e13ddeac482e3bf3240
Kauã Vanceto Costa Motta <86275426+KauaVCMotta@users.noreply.github.com>
2024-01-25 13:26:55 -0300

    ajuste layout

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once('Ler.php');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Black Iphones</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
